c20bed1c422f4bcf1ea0a2555c3f5b5d7e725858
Tom Wilson <tom@hyper.io>
2022-01-14 12:47:01 +0000

    update deploy

--- deploy.js
+++ deploy.js
@@ -9,22 +9,20 @@ const p = Deno.run({
     '--unstable',
     '--no-check=remote',
     'server.js'
-  ],
-  stdout: 'piped',
-  stderr: 'piped'
+  ]
 })
 
 const { code } = p.status()
 
-// Reading the outputs closes their pipes
-const rawOutput = await p.output();
-const rawError = await p.stderrOutput();
+// // Reading the outputs closes their pipes
+// const rawOutput = await p.output();
+// const rawError = await p.stderrOutput();
 
-if (code === 0) {
-  await Deno.stdout.write(rawOutput);
-} else {
-  const errorString = new TextDecoder().decode(rawError);
-  console.log(errorString);
-}
+// if (code === 0) {
+//   await Deno.stdout.write(rawOutput);
+// } else {
+//   const errorString = new TextDecoder().decode(rawError);
+//   console.log(errorString);
+// }
 
-Deno.exit(code);
+// Deno.exit(code);
